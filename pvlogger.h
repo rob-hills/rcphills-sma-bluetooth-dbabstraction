8c08dcc8c0524e0788ff32e8941b80a2ab70b48c
rcp.hills <rcp.hills@localhost>
2012-07-04 01:48:22 +0800

    Extracted hexdump, sunlight; updated Makefile; Added new logging facility; Most of these code changes were merged in from Andreas Florath's sf-pvlog clone: http://code.google.com/r/sf-pvlog/source/detail?r=a9767c1255d6a0968e12b2a8caac6613965f58a3 http://code.google.com/r/sf-pvlog/source/detail?r=e4ae05e701d0a30c809e13c0e25567953b426101 http://code.google.com/r/sf-pvlog/source/detail?r=2eadc7c5c47dd5a1e1c666637505b2474d8deb26 http://code.google.com/r/sf-pvlog/source/detail?r=13a66f6145ff0d7c284f2548f2f36d9b834dc084 http://code.google.com/r/sf-pvlog/source/detail?r=c04d3a75f7bd87f5e6c4e3362b18cc3b9bc89b74 http://code.google.com/r/sf-pvlog/source/detail?r=20d3bdd5bde6ee41f84e15714bb4e68446221af8

---
+++ pvlogger.h
@@ -0,0 +1,39 @@
+/* tool to read power production data for SMA solar power convertors
+   Copyright Wim Hofman 2010
+   Copyright Stephen Collier 2010,2011
+   Copyright flonatel GmbH & Co. KG, 2012
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+#ifndef PVLOGGER_HH
+#define PVLOGGER_HH
+
+#define _XOPEN_SOURCE
+#define _BSD_SOURCE
+#include <time.h>
+
+char * sunset( float latitude, float longitude );
+char * sunrise( float latitude, float longitude );
+
+/* bluetooth.c */
+
+# if 0
+int read_bluetooth(time_t const bt_timeout, int const sfd, int *rr,
+                unsigned char *received, int cc, unsigned char *last_sent,
+                int *terminated );
+void fix_length_received(unsigned char *received, int *len);
+#endif
+
+#endif
+
